39013f0f01abb8beacf05ec20cc2629649707f26
Peadar Grant <peadargrant@gmail.com>
2016-11-25 20:56:10 +0000

    auto table location to fix org-mode ID loss

--- autoprint.py
+++ autoprint.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import datetime
 import calendar
 from lxml import etree
@@ -6,13 +6,17 @@ from lxml.etree import HTMLParser
 from os import system
     
 def main():
-    parser = ArgumentParser()
+    parser = ArgumentParser(
+        description='print linked PDF document from org-mode-generated table',
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("-t", "--test", help="test mode, just print commands", default=False, action='store_true')
     parser.add_argument("-d", "--date", help="override date <YYYY-MM-DD DDD>", default=None)
     parser.add_argument("-f", "--file", help="input file", default="index.html")
     parser.add_argument("-n", "--ncopies", help="number of copies", default=1, type=int)
     parser.add_argument("-s", "--scan", help="scan column", default=2, type=int)
     parser.add_argument("-c", "--column", help="target column index", default=4, type=int)
+    parser.add_argument("-a", "--auto", help="automatically use first table", default=False, action="store_true")
     parser.add_argument("-i", "--id", help="table id", default="orgtable1")
     options = vars(parser.parse_args())
 
@@ -26,7 +30,11 @@ def main():
     index_tree = etree.parse(index_file, parser)
 
     try:
-        today_row = index_tree.xpath("//table[@id='%s']//td[%d]/span/span[contains(text(), '%s')]/../../.." % (options['id'], options['scan'], today))[0]
+        if options['auto']:
+            table_id_predicate = "1"
+        else:
+            table_id_predicate = "@id=\"%s\"" % options['id']
+        today_row = index_tree.xpath("//table[%s]//td[%d]/span/span[contains(text(), '%s')]/../../.." % (table_id_predicate, options['scan'], today))[0]
     except:
         print("not found")
         exit(0)
